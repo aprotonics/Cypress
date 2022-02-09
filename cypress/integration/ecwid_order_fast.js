91a0419a58b0f4e0c079a0cdae47c01bd10b9e60
aprotonics <aprotonics@yandex.ru>
2022-02-09 20:30:27 +0500

    add ecwid_order_fast test

--- cypress/integration/ecwid_order_fast.js
+++ cypress/integration/ecwid_order_fast.js
@@ -2,38 +2,15 @@ describe('Ecwid order test suit', () => {
     it('Ecwid order test', () => {
 
         cy.visit({
-            url: 'https://buy-in-10-seconds.company.site/',
+            url: 'https://buy-in-10-seconds.company.site/Tovar-1-p351702553',
             method: 'GET',
         })
-
-        cy.get('div.grid__products').then(($products) => {
-
-            const products_array = $products.find('a.grid-product__image')
-
-            const product = products_array[1]
-                                                            
-            cy.get(product)
-            .should('be.visible')
-            .click()
-            .url().should('include', '/Tovar')
-
-        }) 
-
-        // cy.get('#static-html > div:nth-child(1) > div > div > div > div > div > div > div > div.ec-footer > ul > li:nth-child(1) > a')
-        // .should('be.visible')
-        // .click()
-        // .url().should('include', '/search')
-
-        // cy.get('.grid__products > div:first-child div.grid-product__shadow')
-        // .should('be.visible')
-        // .click()
-        // .url().should('include', '/Tovar')
 
         cy.contains('В корзину')
         .should('be.visible')
         .click()
 
-        cy.get('div.float-icons__wrap > div:first-child svg.icon-default')
+        cy.get('.float-icons__icon--cart > .ec-cart-widget > .ec-minicart')
         .should('be.visible')
         .click()
         .url().should('include', '/cart')
@@ -43,11 +20,10 @@ describe('Ecwid order test suit', () => {
         .should('have.text', 'Оформление заказа')
 
         cy.get('#ec-cart-email-input')
-        .focus()
         .type('test@mail.ru', { delay: 25 })
         .should('have.value', 'test@mail.ru')
         
-        cy.get('input#form-control__checkbox--agree')
+        cy.get('#form-control__checkbox--agree')
         .should('be.enabled')
         .check()
         .should('be.checked')
@@ -85,24 +61,6 @@ describe('Ecwid order test suit', () => {
         .should('be.visible')
         .click()
         .url().should('include', '/checkout/order-confirmation')
-
-        // cy.get('div.ec-cart-step__title.ec-header-h4')
-        // .should('be.visible')
-        // .should('have.text', 'Способ доставки')
-
-        // cy.contains('Продолжить')
-        // .should('be.visible')
-        // .click()
-        // .url().should('include', '/checkout/payment')
-
-        // cy.get('div.ec-cart-step__title.ec-header-h4')
-        // .should('be.visible')
-        // .should('have.text', 'Оплата')
-
-        // cy.contains('Разместить заказ')
-        // .should('be.visible')
-        // .click()
-        // .url().should('include', '/checkout/order-confirmation')
 
         cy.get('div.ec-store__confirmation-page h1.page-title__name')
         .should('be.visible')
